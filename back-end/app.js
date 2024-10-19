91209459c29b382cdd97342bb892d6a105fb5091
Riadh Boudali <r.boudali@esi-sba.dz>
2024-10-19 23:01:58 +0100

    Fetching dummy posts

--- back-end/app.js
+++ back-end/app.js
@@ -6,10 +6,10 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json()); // a middleware for body-parser for content-type of application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // to set a header for Access-Control-Allow-Origin to prevent CORS errors
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
