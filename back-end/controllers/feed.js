d4d65fd28eeaf42570138104d8a944b937952e1f
Riadh Boudali <r.boudali@esi-sba.dz>
2024-10-26 19:11:15 +0100

    Post-User relation cleard

--- back-end/controllers/feed.js
+++ back-end/controllers/feed.js
@@ -176,8 +176,16 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndDelete(postId);
     })
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => {
       res.status(200).json({
         message: "Post Deleted!",
+        result: result,
       });
     })
     .catch((err) => {
