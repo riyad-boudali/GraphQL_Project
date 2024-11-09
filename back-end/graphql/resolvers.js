626ab1150810f3c4819f71044508b92296e191fe
Riadh Boudali <r.boudali@esi-sba.dz>
2024-11-09 10:49:10 +0100

    adding login query & a resolver

--- back-end/graphql/resolvers.js
+++ back-end/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -31,5 +32,29 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() }; // createdUser._doc will return the user doc without the additional metadata added by mongoose
+  },
+  
+  login: async function ({ email, password }) {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User not found!");
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      "secret",
+      { expiresIn: "1h" }
+    );
+    return { token: token, userId: user._id.toString() };
   },
 };
