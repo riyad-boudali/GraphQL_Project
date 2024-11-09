b6cb9b2180145c63ce0846b5edf8f7424098fcca
Riadh Boudali <r.boudali@esi-sba.dz>
2024-11-09 17:15:24 +0100

    Adding get_posts query resolver

--- back-end/graphql/resolvers.js
+++ back-end/graphql/resolvers.js
@@ -59,9 +59,9 @@ module.exports = {
     return { token: token, userId: user._id.toString() };
   },
 
-  createPost: async function({ postInput }, req) {
+  createPost: async function ({ postInput }, req) {
     if (!req.isAuth) {
-      const error = new Error('Not authenticated!');
+      const error = new Error("Not authenticated!");
       error.code = 401;
       throw error;
     }
@@ -70,23 +70,23 @@ module.exports = {
       validator.isEmpty(postInput.title) ||
       !validator.isLength(postInput.title, { min: 5 })
     ) {
-      errors.push({ message: 'Title is invalid.' });
+      errors.push({ message: "Title is invalid." });
     }
     if (
       validator.isEmpty(postInput.content) ||
       !validator.isLength(postInput.content, { min: 5 })
     ) {
-      errors.push({ message: 'Content is invalid.' });
+      errors.push({ message: "Content is invalid." });
     }
     if (errors.length > 0) {
-      const error = new Error('Invalid input.');
+      const error = new Error("Invalid input.");
       error.data = errors;
       error.code = 422;
       throw error;
     }
     const user = await User.findById(req.userId);
     if (!user) {
-      const error = new Error('Invalid user.');
+      const error = new Error("Invalid user.");
       error.code = 401;
       throw error;
     }
@@ -94,7 +94,7 @@ module.exports = {
       title: postInput.title,
       content: postInput.content,
       imageUrl: postInput.imageUrl,
-      creator: user
+      creator: user,
     });
     const createdPost = await post.save();
     user.posts.push(createdPost);
@@ -103,7 +103,29 @@ module.exports = {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
       createdAt: createdPost.createdAt.toISOString(),
-      updatedAt: createdPost.updatedAt.toISOString()
+      updatedAt: createdPost.updatedAt.toISOString(),
     };
   },
+  posts: async function(args, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const totalPosts = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate('creator');
+    return {
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
+        };
+      }),
+      totalPosts: totalPosts
+    };
+  }
 };
